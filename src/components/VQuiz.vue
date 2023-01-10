1b195538a569aaa2a73880fb7622c8a3beb351dc
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2023-01-10 20:15:30 +0900

    add css

--- src/components/VQuiz.vue
+++ src/components/VQuiz.vue
@@ -2,86 +2,50 @@
   <div class="quiz">
     <div class="content">
       <div class="circle" :class="{ animation: isSlide }">
-        <span style="--i: 1"
-          ><div class="sub">
+        <div class="slide" v-for="n in 7" :key="n" :style="{ '--i': n }">
+          <div class="sub">
             <img
               class="image"
               src="../assets/image/animal_study_usagi.png"
               alt=""
-            /></div
-        ></span>
-        <span style="--i: 2"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 3"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 4"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 5"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 6"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 7"
-          ><div class="sub">
-            <img
-              class="image"
-              src="../assets/image/animal_study_usagi.png"
-              alt=""
-            /></div
-        ></span>
-        <span style="--i: 8"
-          ><div class="back" :class="{ animationOpen: isTurn }">
-            <h2>解説</h2>
+            />
+          </div>
+        </div>
+        <div class="slide" style="--i: 8">
+          <div class="back" :class="{ animationOpen: isTurn }">
+            <h2 class="title">解説</h2>
             <button class="button" @click="doAnime(700)">次の問題</button>
-          </div></span
-        >
-        <span style="--i: 8"
-          ><div class="main">
-            <h2>問題</h2>
+          </div>
+        </div>
+        <div class="slide" style="--i: 8">
+          <div class="main">
+            <h2 class="title">問題</h2>
             <div class="sentence-wrapper">
-              <h2 class="text">{{ data[0].text }}</h2>
-              <ul class="choice-wrapper">
-                <button class="choice">ア{{ data[0].choice[0] }}</button>
-                <button class="choice">イ{{ data[0].choice[1] }}</button>
-                <button class="choice">ウ{{ data[0].choice[2] }}</button>
-                <button class="choice">エ{{ data[0].choice[3] }}</button>
-              </ul>
+              <h2 class="text">{{ data[4].text }}</h2>
+              <div class="choice-wrapper">
+                <div
+                  class="choice"
+                  v-for="(choice, index) in data[1].choice"
+                  :key="index"
+                >
+                  <button class="choice-button" v-if="index === 0">ア</button>
+                  <button class="choice-button" v-else-if="index === 1">
+                    イ
+                  </button>
+                  <button class="choice-button" v-else-if="index === 2">
+                    ウ
+                  </button>
+                  <button class="choice-button" v-else>エ</button>
+                  <h2 class="choice-text">{{ choice }}</h2>
+                </div>
+              </div>
             </div>
             <div class="button-wrapper">
               <button class="button" @click="doAnime(2000)">解説</button>
               <button class="button" @click="doAnime(700)">次の問題</button>
             </div>
-          </div></span
-        >
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -98,11 +62,13 @@ export default defineComponent({
       isTurn: boolean;
       interval: number | null;
       data: Dummydata[];
+      divCount: number;
     } = reactive({
       isSlide: false,
       isTurn: false,
       interval: null,
       data: dummydata,
+      divCount: 0,
     });
 
     const doAnime = (second: 700 | 2000) => {
@@ -118,7 +84,13 @@ export default defineComponent({
       second == 700 && (state.isTurn = false);
     };
 
-    return { ...toRefs(state), doAnime };
+    const style = () => {
+      state.divCount += 1;
+      console.log(state.divCount);
+      return { "--i": state.divCount + 1 };
+    };
+
+    return { ...toRefs(state), doAnime, style };
   },
 });
 </script>
@@ -180,7 +152,7 @@ export default defineComponent({
       @include mq(md) {
         width: 400px;
       }
-      span {
+      .slide {
         width: 100%;
         position: absolute;
         top: 0;
@@ -194,11 +166,13 @@ export default defineComponent({
         @include mq(md) {
           transform: rotateY(calc(var(--i) * 45deg)) translateZ(550px);
         }
-        div {
+        .sub {
           width: 100%;
           height: 100%;
           border: 1px solid;
           @include flexCenter;
+          background-color: white;
+
           .image {
             width: 50%;
             height: 50%;
@@ -206,81 +180,96 @@ export default defineComponent({
             object-fit: contain;
           }
         }
-      }
-      .sub {
-        background: white;
+        .main {
+          width: 100%;
+          height: 100%;
+          border: 1px solid;
+          background: white;
+          height: 100%;
+          position: relative;
+          h2 {
+            font-family: "ZenMaru-light";
+            font-size: medium;
+            left: 0;
+            right: 0;
+            margin: auto;
+            text-align: center;
+            height: 9%;
+          }
+          .sentence-wrapper {
+            position: relative;
+            height: 91%;
+            padding: 0 6px;
+            .text {
+              font-size: 1vh;
+              height: 50%;
+              margin: 0;
+              padding: 0;
+              vertical-align: top;
+            }
+            .choice-wrapper {
+              width: 100%;
+              height: 40%;
+              bottom: 15px;
+              display: flex;
+              flex-wrap: wrap;
+              padding: 0;
+              .choice {
+                width: 50%;
+                display: flex;
+                @include mq(tb) {
+                  font-size: x-small;
+                }
+                .choice-button {
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  pointer-events: auto;
+                  font-size: 1vh;
+                  text-align: center;
+                  font-weight: bold;
+                  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
+                  border: none;
+                  transition: 0.2s;
+                  &:active {
+                    transform: translateY(2px);
+                    box-shadow: 0 0 1px rgba(0, 0, 0, 0.15);
+                  }
+                }
+                .choice-text {
+                  font-size: 1vh;
+                  margin: 0;
+                }
+              }
+            }
+          }
+          .button-wrapper {
+            width: 100%;
+            position: absolute;
+            bottom: 4px;
+            display: flex;
+            justify-content: center;
+            .button {
+              @include buttonCenter(23px, 90px, xx-small);
+              pointer-events: auto;
+            }
+          }
+        }
       }
       .animationOpen {
         animation: animate359 3s;
         transform-origin: top;
         animation-fill-mode: forwards; // 最後の状態
       }
-      .main {
-        background: white;
-        display: block;
-        height: 100%;
-        position: relative;
-        h2 {
-          position: absolute;
-          font-family: "ZenMaru-light";
-          font-size: medium;
-          left: 0;
-          right: 0;
-          margin: auto;
-          text-align: center;
-        }
-        div {
-          position: relative;
-          .text {
-            @include mq(tb) {
-              font-size: medium;
-            }
-            font-size: x-small;
-            position: absolute;
-            top: 20px;
-          }
-          .choice-wrapper {
-            position: absolute;
-            bottom: 15px;
-            display: flex;
-
-            flex-wrap: wrap;
-            padding: 0;
-
-            .choice {
-              width: 50%;
-              list-style: none;
-              text-align: left;
-              background-color: white;
-              @include buttonCenter(23px, 50%, xx-small);
-              pointer-events: auto;
-              margin: 2px 0;
-              font-size: 10px;
-              transform: scale(0.8);
-              transform-origin: 0, 0;
-              @include mq(tb) {
-                font-size: x-small;
-              }
-            }
-          }
-        }
-        .button-wrapper {
-          position: absolute;
-          border: none;
-          height: fit-content;
-          bottom: 4px;
-          .button {
-            @include buttonCenter(23px, 100px, xx-small);
-            pointer-events: auto;
-          }
-        }
-      }
       .back {
+        width: 100%;
+        height: 100%;
         background: white;
         height: 100%;
         position: relative;
+        border: 1px solid;
         .button {
-          @include buttonCenter(23px, 120px, xx-small);
+          @include buttonCenter(23px, 100px, xx-small);
           transform: translateY();
           pointer-events: auto;
           position: absolute;
