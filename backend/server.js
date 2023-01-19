a6f53b34c3f83ad6a8bd52487b35a700ddccc3f4
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2023-01-20 01:46:16 +0900

    test

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,63 @@ const authRoute = require("./routes/auth");
 const app = express();
 const cors = require("cors");
 
-const PORT = process.env.FRONT_URL;
+const PORT = process.env.FRONT_URL || 3000;
 
 app.use(
   cors({
     origin: `${process.env.FRONT_URL}`, //アクセス許可するオリジン
   })
 );
+
+app.post("/register", async (req, res) => {
+  const regex =
+    /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*.)+[a-zA-Z]{2,}$/;
+  try {
+    if (!regex.test(req.body.email)) {
+      return res.status(500).json("メールアドレスの形式が違います");
+    } else if (req.body.password.length < 5) {
+      return res.status(500).json("5文字以上のパスワードを入力してください");
+    } else {
+      const sql = "INSERT INTO users(name,email,password) VALUES(?,?,?)";
+      const params = [req.body.name, req.body.email, req.body.password];
+      con.query(sql, params, function (err, result, fields) {
+        if (err) {
+          if (err.code == "ER_DUP_ENTRY" || err.errno == 1062) {
+            return res
+              .status(500)
+              .json("すでにそのメールアドレスで登録しています");
+          } else if (err.code == "ER_BAD_NULL_ERROR") {
+            return res.status(500).json(err.sqlMessage);
+          } else {
+            return res.status(500).json("何かのエラーがあります");
+          }
+        } else {
+          return res.status(200).json("成功");
+        }
+      });
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const sql = "SELECT id, password, name  FROM users WHERE email = ?";
+  const params = req.body.email;
+  con.query(sql, params, function (err, result, fields) {
+    try {
+      const password = result[0].password;
+      if (password == req.body.password) {
+        return res.status(200).json({ id: result[0].id, name: result[0].name });
+      } else {
+        return res.status(500).json("パスワードが違います");
+      }
+    } catch (err) {
+      // return res.status(500).json("メールアドレスが違います");
+      return res.status(500).json(err);
+    }
+  });
+});
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
 const con = mysql.createConnection({
@@ -27,7 +77,7 @@ app.get("/", (req, res) => {
 //登録
 
 app.use(express.json());
-app.use("/api/auth", authRoute);
+// app.use("/api/auth", authRoute);
 
 module.exports = app;
 // app.use(function (req, res, next) {
