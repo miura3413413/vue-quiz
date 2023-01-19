2f117d9046a1f95e224b225fcb770dc74fcdc4fd
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2023-01-19 18:07:46 +0900

    test

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const authRoute = require("./routes/auth");
 const app = express();
 const cors = require("cors");
 
-const PORT = 3000;
+const PORT = process.env.FRONT_URL || 3000;
 
 app.use(
   cors({
