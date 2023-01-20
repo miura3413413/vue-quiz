d2dd9df5c4b1ae5727eac9fac24614f6551fa537
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2023-01-20 18:24:56 +0900

    render test

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: "express_db",
+});
+
 // const PORT = 3001;
 
 app.use(
@@ -24,7 +31,25 @@ app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
+app.post("/auth/api/login", async (req, res) => {
+  const sql = "SELECT id, password, name  FROM users WHERE email = ?";
+  const params = req.body.email;
+  con.query(sql, params, function (err, result, fields) {
+    try {
+      const password = result[0].password;
+      if (password == req.body.password) {
+        return res.status(200).json({ id: result[0].id, name: result[0].name });
+      } else {
+        return res.status(500).json("パスワードが違います");
+      }
+    } catch (err) {
+      // return res.status(500).json("メールアドレスが違います");
+      return res.status(500).json(err);
+    }
+  });
+});
+
 app.use(express.json());
-app.use("/api/auth", authRoute);
+// app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log("サーバーが起動しました", PORT));
